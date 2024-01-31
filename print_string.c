2d07d10c649651e7c9c9b9e89fe10b6f732fe27b
MDederi <mjdederi01@gmail.com>
2024-01-30 16:04:53 -0800

    print_string.c

--- print_string.c
+++ print_string.c
@@ -1,20 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
- * print_string - writes the string str to the stdout
- * @str: The string to print
- * @count: The number of charcters printed
+ * print_string - writes the character c to stdout
+ * @s: The string to print
  *
- * Return: 1
+ * Return: 1.
  */
-
-int print_string(const char *str, int *count)
+int print_string(va_list s)
 {
-	while (*str != '\0')
-	{
-		print_char(*str, count);
-		str++;
-	}
+	char *my_string;
+	int  i = 0;
 
-	return (1);
+	my_string = va_arg(s, char *);
+	if (my_string == NULL)
+		my_string = "(null)";
+	while (my_string[i])
+	{
+		_putchar(my_string[i]);
+		i++;
+	}
+	return (i);
 }
